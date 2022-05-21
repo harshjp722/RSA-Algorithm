a52c10fee48190a970fb5b7ad996186e65afa5e8
Hp <harshp38@gmail.com>
2022-05-21 13:16:36 +0530

    Removed unnecessary imports

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
-using System.IO;
 
 namespace ConsoleApp1
 {
